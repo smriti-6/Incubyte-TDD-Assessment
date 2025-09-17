7a4edd499d61c50b2f84bec89a30ce3c35b3b92c
Smriti <smritikanthak@gmail.com>
2025-09-17 23:40:38 +0530

    Support single number input

--- string_calculator_TDD.py
+++ string_calculator_TDD.py
@@ -1,6 +1,8 @@
 def add(numbers: str) -> int:  # Implementation: input is a string and output is an integer 
  if numbers == "":      # empty string 
    return 0 
+ else:
+     return int(numbers)
 
 
      
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -3,4 +3,5 @@ from string_calculator_TDD import add
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(add(""),0)
-               
+    def test_single_number(self):
+        self.assertEqual(add("5"),5)       
