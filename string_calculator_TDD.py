1054f513bfb3e46d44598d6f893efd7e581511e9
Smriti <smritikanthak@gmail.com>
2025-09-18 01:41:52 +0530

    Handle newline delimiter and skip empty strings

--- string_calculator_TDD.py
+++ string_calculator_TDD.py
@@ -1,13 +1,27 @@
 def add(numbers: str) -> int:  # Implementation: input is a string and output is an integer 
- if numbers == "":      # empty string 
-   return 0 
- elif "," not in numbers:
-     return int(numbers)
- else:
-     total = 0
-     for num in numbers.split(","):
-         total+=int (num)
-     return total
+    if numbers == "":      # empty string 
+        return 0 
+
+    delimiter = ","   # default delimiter
+    if numbers.startswith("//"):
+      delimiter = numbers[2]        # "//;\n" → ";" at index 2
+      numbers = numbers[4:]         # skip "//;\n"
+
+    numbers = numbers.replace("\n", delimiter)
+
+    if delimiter not in numbers:
+      return int(numbers)
+
+    total = 0
+    for num in numbers.split(delimiter):
+        if num:
+            total+=int(num)
+    return total
+
+ 
+
+ 
+ 
 
     
      
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -9,3 +9,5 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(add("1,2"),3)      
     def test_newline_delimiter(self):
         self.assertEqual(add("1,\n2,3"),6)
+    def test_custom_delimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
